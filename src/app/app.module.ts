d27f601a978053f36a9c14c15273e3e4af5e694f
LILLK-DAW2 <tahrat.mehdi.co@alumnes.ilernalleida.com>
2024-02-22 19:02:27 +0100

    a
    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './componentes/component/component.component';
+import { PaginaComponent } from './Paginas/pagina/pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    PaginaComponent
   ],
   imports: [
     BrowserModule,

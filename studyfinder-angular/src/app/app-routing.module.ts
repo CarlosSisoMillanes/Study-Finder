73595789fcae2274df53a0a932e8081197fdf456
Orteeuu <rorteub02@gmail.com>
2024-03-20 19:05:35 +0100

    huh

--- studyfinder-angular/src/app/app-routing.module.ts
+++ studyfinder-angular/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Paginas/home/home.component';
 
-
+const routes: Routes = [
+  { path: 'welcome', component: HomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
